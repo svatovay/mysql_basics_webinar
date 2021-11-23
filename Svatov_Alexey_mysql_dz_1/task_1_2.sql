b8dde56cae90ef3c3b38f1bd0db7a05bd1f71459
Alexey <zombeaver@gmail.com>
2021-11-23 20:52:57 +0500

    done task_1_2

---
+++ Svatov_Alexey_mysql_dz_1/task_1_2.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE example;
+USE example
+CREATE TABLE users (id INT, name VARCHAR(20), PRIMARY KEY (id));
+DESC users
+/*
++-------+-------------+------+-----+---------+-------+
+| Field | Type        | Null | Key | Default | Extra |
++-------+-------------+------+-----+---------+-------+
+| id    | int         | NO   | PRI | NULL    |       |
+| name  | varchar(20) | YES  |     | NULL    |       |
++-------+-------------+------+-----+---------+-------+
+*/
