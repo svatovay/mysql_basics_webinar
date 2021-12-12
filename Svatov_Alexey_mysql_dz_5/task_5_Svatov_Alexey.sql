30b43e10d2c6deddd62e4acf5827980cd58c6029
Alexey <zombeaver@gmail.com>
2021-12-12 12:02:51 +0500

    done_task_5_2

--- Svatov_Alexey_mysql_dz_5/task_5_Svatov_Alexey.sql
+++ Svatov_Alexey_mysql_dz_5/task_5_Svatov_Alexey.sql
@@ -14,5 +14,35 @@ UPDATE `users` SET `created_at`=NOW() WHERE ISNULL(`created_at`)
 
 UPDATE `users` SET `updated_at`=NOW() WHERE ISNULL(`updated_at`)
 
--- task_5_2
+-- task_5_2 (есть решение на https://question-it.com/questions/677879/mysql-konvertirovat-dannye-varchar-v-datetime)
+
+ALTER TABLE `users` 
+ADD COLUMN `created_at_v` VARCHAR(20) DEFAULT '20.10.2017 8:10'
+AFTER `updated_at`;
+
+ALTER TABLE `users` 
+ADD COLUMN `updated_at_v` VARCHAR(20) DEFAULT '20.10.2017 8:10'
+AFTER `created_at_v`;
+
+ALTER TABLE `users`
+ADD COLUMN `created_at_dt` DATETIME DEFAULT NULL
+AFTER `created_at_v`;
+
+UPDATE `users` SET `created_at_dt`=STR_TO_DATE(`created_at_v`, '%d.%m.%Y %h:%i');
+
+ALTER TABLE `users`
+ADD COLUMN `updated_at_dt` DATETIME DEFAULT NULL
+AFTER `updated_at_v`;
+
+UPDATE `users` SET `updated_at_dt`=STR_TO_DATE(`updated_at_v`, '%d.%m.%Y %h:%i');
+
+ALTER TABLE `users` 
+DROP `created_at_v`, DROP `updated_at_v`; 
+
+-- task_5_3
+
+
+
+
+
 
