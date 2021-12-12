eed9bcf589134e81be94dd28bfa40f798d301c3e
Alexey <zombeaver@gmail.com>
2021-12-12 12:36:29 +0500

    done_task_5_3

--- Svatov_Alexey_mysql_dz_5/task_5_Svatov_Alexey.sql
+++ Svatov_Alexey_mysql_dz_5/task_5_Svatov_Alexey.sql
@@ -41,8 +41,42 @@ DROP `created_at_v`, DROP `updated_at_v`;
 
 -- task_5_3
 
+USE shop;
 
+INSERT INTO `storehouses_products` VALUES
+(1, NULL, NULL, 0, NULL, NULL),
+(2, NULL, NULL, 1000, NULL, NULL),
+(3, NULL, NULL, 1500, NULL, NULL),
+(4, NULL, NULL, 1250, NULL, NULL),
+(5, NULL, NULL, 0, NULL, NULL),
+(6, NULL, NULL, 0, NULL, NULL),
+(7, NULL, NULL, 125, NULL, NULL),
+(8, NULL, NULL, 0, NULL, NULL),
+(9, NULL, NULL, 0, NULL, NULL),
+(10, NULL, NULL, 600, NULL, NULL),
+(11, NULL, NULL, 0, NULL, NULL),
+(12, NULL, NULL, 0, NULL, NULL),
+(13, NULL, NULL, 0, NULL, NULL),
+(14, NULL, NULL, 0, NULL, NULL),
+(15, NULL, NULL, 7000, NULL, NULL),
+(16, NULL, NULL, 2500, NULL, NULL),
+(17, NULL, NULL, 30, NULL, NULL),
+(18, NULL, NULL, 500, NULL, NULL),
+(19, NULL, NULL, 0, NULL, NULL),
+(20, NULL, NULL, 1, NULL, NULL)
+;
 
+SELECT `value` FROM `storehouses_products` ORDER BY `value`=0, `value`;
+
+-- task_5_4
+
+-- task_5_5
+
+-- task_aggregation_1
+
+-- task_aggregation_2
+
+-- task_aggregation_3
 
 
 
