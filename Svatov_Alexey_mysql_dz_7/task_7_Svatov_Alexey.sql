c8e995bac1c152da0c595d7f0d1c386a7781cd51
Alexey <zombeaver@gmail.com>
2021-12-19 12:57:06 +0500

    done task_7_1

--- Svatov_Alexey_mysql_dz_7/task_7_Svatov_Alexey.sql
+++ Svatov_Alexey_mysql_dz_7/task_7_Svatov_Alexey.sql
@@ -1,6 +1,26 @@
 -- task_7_1
 
+USE `shop`
 
+INSERT INTO `orders` (user_id) VALUES
+  (1),
+  (1),
+  (1),
+  (3),
+  (6),
+  (6),
+  (1),
+  (2),
+  (1),
+  (1),
+  (3),
+  (1),
+  (5),
+  (1);
+
+SELECT users.name, COUNT(user_id) AS cnt_orders
+FROM `users` JOIN `orders` ON users.id = orders.user_id 
+GROUP BY user_id 
 
 -- task_7_2
 
